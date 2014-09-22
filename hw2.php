cce192d7094157b8e1b375171298e13f4a293b96
kimminahm <mdkim105@gmail.com>
2014-09-22 15:45:25 -0700

    Commiting initial files

---
+++ hw2.php
@@ -0,0 +1,35 @@
+<? ob_start() ?>
+<html>
+<head>
+	<title>Order Processed</title>
+	<link rel="stylesheet" href="style.css">
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+<body>
+	<p>
+	<?
+	$page = $_POST['page'];
+	switch($page){
+	case 'order':
+		header("Location: new_order_form.html");
+		break;
+	case 'item':
+		header("Location: item.html");
+		break;
+	case 'customer':
+		header("Location: new_customer_form.html");
+		break;
+	case 'manage':
+		header("Location: manage.html");
+		break;
+	case 'report':
+		header("Location: report.html");
+		break;
+	default:
+	}
+		break;
+	
+	?>
+	
+	</body>
+</html>
