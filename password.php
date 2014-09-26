722813f56de2882c29c410ef3b001a598f18ffde
whoisjamosses <egbertnd@gmail.com>
2014-09-26 14:49:25 -0400

    fuckme.com/fuck
    some validation still needs fixed with a css sheet. figuring out the
    picture validation errors

--- password.php
+++ password.php
@@ -1,7 +1,9 @@
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" 
+ "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<title>Piedmont Furnishings</title>
-	<link rel="stylesheet" href="hw2.css">
+	<link rel="stylesheet" href="hw2.css"/>
 </head>
 <body>
 <center>
