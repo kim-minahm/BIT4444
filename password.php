5ed1a5524141fde74c556a74c5665730f75734dd
kimminahm <mdkim105@gmail.com>
2014-09-25 22:25:43 -0400

    more changes
    formatting and edited home page

--- password.php
+++ password.php
@@ -1,8 +1,11 @@
 <html>
-<head><title>BIT Student Registration</title></head>
-<body bgcolor="#dddddd">
+<head>
+	<title>Piedmont Furnishings</title>
+	<link rel="stylesheet" href="hw2.css">
+</head>
+<body>
 <center>
-<h1>Business Information Technology<br />Student Registration</h1>
+<h1>Piedmont Furnishings</h1>
 <hr />
 </center>
 <br /><br /><br />
@@ -10,45 +13,59 @@
 <? if (isset($_REQUEST['page1_submit'])) {
 // __________________________________________________ DISPLAY PAGE 2  ?>
 
-<? if ($_REQUEST['pword'] == "meatball") { ?>
-<center><h2>You entered the correct password on the first try!</h2>
-      	<img src="../../images/vt_icon.gif" border="2" /></center>
-<? } else { ?>
-       <center><h2>Wrong Password! Try again.
-       <form method="POST" action="password.php">
-Password: <input type="password" NAME="pword" /><br /><br />
-<input type="submit" name="page2_submit" value="SUBMIT" />
-<input type="reset" value="RESET" /><br />
+	<? if ($_REQUEST['pword'] == "furnish") { ?>
+		<center><h2>You entered the correct password on the first try!</h2>
+      	</center>
+		<?header("Location: userpage.html");?>
+	<? } else if ($_REQUEST['pword'] == "manager") { ?>
+		<center><h2>You entered the correct password on the first try!</h2>
+      	</center>
+		<?header("Location: managerpage.html");?>
+	<? } else { ?>
+		<center><h2>Wrong Password! Try again.
+		<form method="POST" action="password.php">
+			Password: <input type="password" NAME="pword" /><br /><br />
+			<input type="submit" name="page2_submit" value="SUBMIT" />
+			<input type="reset" value="RESET" /><br />
        </form>
-       </h2></center>
-<? } ?>
+	</h2></center>
+	<? } ?>
 
 <? } elseif (isset($_REQUEST['page2_submit'])) {
 // __________________________________________________ DISPLAY PAGE 3  ?>
 
-<? if ($_REQUEST['pword'] == "meatball") { ?>
-<center><h2>You entered the correct password!  But it took you two tries.</h2>
-<img src="../../images/vt_icon.gif" border="2" /></center>
-<? } else { ?>
-<center><h2>Wrong Password Again! This was your second try.
-<form method="POST" action="password.php">
-Password: <input type = "password" name="pword" /><br /><br />
-<input type="submit" name="page3_submit" value="SUBMIT" />
-<input type="reset" value="RESET" /><br />
-</form>
-</h2></center>
+	<? if ($_REQUEST['pword'] == "furnish") { ?>
+		<center><h2>You entered the correct password on the first try!</h2>
+      	</center>
+		<?header("Location: userpage.html");?>
+	<? } else if ($_REQUEST['pword'] == "manager") { ?>
+		<center><h2>You entered the correct password on the first try!</h2>
+      	</center>
+		<?header("Location: managerpage.html");?>
+	<? } else { ?>
+		<center><h2>Wrong Password Again! This was your second try.
+		<form method="POST" action="password.php">
+			Password: <input type = "password" name="pword" /><br /><br />
+			<input type="submit" name="page3_submit" value="SUBMIT" />
+			<input type="reset" value="RESET" /><br />
+		</form>
+	</h2></center>
 <? } ?>
 
 <? } elseif (isset($_REQUEST['page3_submit'])) {
 // ________________________________________________ DISPLAY FINAL PAGE  ?>
 
-<? if ($_REQUEST['pword'] == "meatball") { ?>
-<center><h2>You entered the correct password!   But it took you three tries.</h2>
-<img src="../../images/vt_icon.gif" border="2" /></center>
-<? } else { ?>
-<center><h2>Wrong Password Again! You're out of luck.</h2></center>
-<? } ?>
-
+	<? if ($_REQUEST['pword'] == "furnish") { ?>
+		<center><h2>You entered the correct password on the first try!</h2>
+      	</center>
+		<?header("Location: userpage.html");?>
+	<? } else if ($_REQUEST['pword'] == "manager") { ?>
+		<center><h2>You entered the correct password on the first try!</h2>
+      	</center>
+		<?header("Location: managerpage.html");?>
+	<? } else { ?>
+		<center><h2>Wrong Password Again! You're out of luck.</h2></center>
+	<? } ?>
 <? } else {
 // ____________________________________________________________ DEFAULT  ?>
 
