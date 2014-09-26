22a32c7680153cf92c9c9796003ea51c216cc1c3
kimminahm <mdkim105@gmail.com>
2014-09-25 21:42:56 -0400

    full of bullshit
    ubllshit

---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML SYSTEM>
+<html>
+<head>
+	<title>Order Processed</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<?
+	$username = $_POST['month'];
+	$password = $_POST['date'];	
+	for(int i = 0; i < 3; i++){
+		if($password == "meatball"){
+			//go to manager page
+		}else if($password == "pigsfeet"){
+			//go to user page
+		}else{?>
+			First Name: <input name="fname" size="48" /><br />
+			Last Name:  <input name="lname" size="48" /><br />
+			<input type="subminnnnnnnnn               t" value="Submit" /><?
+		}
+	}
+	?>
+	</body>
+</html>
