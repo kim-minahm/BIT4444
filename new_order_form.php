3c3e6a5d8e030f5c20f96a36c941e8cad0fd8cdf
minahm <mdkim105@gmail.com>
2014-10-16 08:05:18 -0800

    adding navigation bar throught out site

--- new_order_form.php
+++ new_order_form.php
@@ -6,10 +6,55 @@
 	<link rel="stylesheet" href="hw2.css"/>
 </head>
 <body>
-	<center>
 	<h1><a href="http://tinyurl.com/mstgdqk"><img src="http://tinyurl.com/on58dwh" alt=" photo Untitled_zps8bfcff57.jpg"/></a></h1>
 	<hr/>
-	</center>
+	<h2>New Order Form</h2>
+	<?if($_GET['acct']=="manager"){
+	print '
+	<div id="nav">
+		<h3>Navigation</h3>
+		<form name="form1" method="post" action="hw2.php">
+			<div>
+			<select name="page">
+				<option value="order">New Order Form/Edit Order Form</option>
+				<option value="customer">New Customer Form/Edit Customer</option>
+				<option value="manage">Management Access Only</option>
+				<option value="report">Itemized Sales Report</option>
+				<option value="performance">Performance Report</option>
+				<option value="business">Business Report</option>
+			</select>
+			<input type="hidden" name="acct" value="manager"/>
+			</div>
+			<p><input type="submit" value="Submit" /></p>
+		</form>
+		8:00 a.m. BIT 4444<br/>
+		Group 6 Team Members:<br/>
+			Minahm Kim<br/>
+			Andrew Knittle<br/>
+			Nathan Egbert
+	</div>
+	';}
+	else if($_GET['acct']=="user"){
+	print '
+	<div id="nav">
+		<h3>Navigation</h3>
+		<form name="form1" method="post" action="hw2.php">
+			<div>
+			<select name="page">
+				<option value="order">New Order Form/Edit Order Form</option>
+				<option value="customer">New Customer Form/Edit Customer</option>
+				<option value="item">Itemize sales report</option>
+			</select>
+			<input type="hidden" name="acct" value="user"/>
+			</div>
+			<p><input type="submit" value="Submit" /></p>
+		</form>
+		8:00 a.m. BIT 4444:<br/>
+		Group 6 Team Members:<br/>
+			Minahm Kim<br/>
+			Andrew Knittle<br/>
+			Nathan Egbert
+	</div>';}?>
     <form method="post" action="order_result.php">
 		<input type="hidden" name="acct" value="<?=$_GET['acct'];?>"/>
 		<table>
--- report.php
+++ report.php
@@ -9,6 +9,29 @@
 <center>
 	<h1><a href="http://tinyurl.com/mstgdqk"><img src="http://tinyurl.com/on58dwh" alt=" photo Untitled_zps8bfcff57.jpg"/></a></h1>
 	<hr/>
+	<h2> Order Submitted:</h2>
+		<div id="nav">
+		<h3>Navigation</h3>
+		<form name="form1" method="post" action="hw2.php">
+			<div>
+			<select name="page">
+				<option value="order">New Order Form/Edit Order Form</option>
+				<option value="customer">New Customer Form/Edit Customer</option>
+				<option value="manage">Management Access Only</option>
+				<option value="report">Itemized Sales Report</option>
+				<option value="performance">Performance Report</option>
+				<option value="business">Business Report</option>
+			</select>
+			<input type="hidden" name="acct" value="manager"/>
+			</div>
+			<p><input type="submit" value="Submit" /></p>
+		</form>
+		8:00 a.m. BIT 4444<br/>
+		Group 6 Team Members:<br/>
+			Minahm Kim<br/>
+			Andrew Knittle<br/>
+			Nathan Egbert
+	</div>
 </center>
 	<body>
 	
