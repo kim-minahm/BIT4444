e055281682260d40569fac4f4d1f4cf3cf0db864
minahm <mdkim105@gmail.com>
2014-10-25 23:04:35 -0800

    Made new home page
    Made a new home page that displays the name and appropriate page
    depending on account

---
+++ home.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" 
+ "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<title>Manager Portal</title>
+	<link rel="stylesheet" href="hw2.css"/>
+	<?session_start()?>
+</head>
+<body>
+	<h1><a href="http://tinyurl.com/mstgdqk"><img src="http://tinyurl.com/on58dwh" alt=" photo Untitled_zps8bfcff57.jpg"/></a></h1>
+	<hr/>
+	<?if($_SESSION['account'] == "manager"){print '
+	<h2>Manager Portal</h2>
+	<h4>Welcome '.$_SESSION['name'].'</h4>
+	<div id="nav">
+		<h3>Navigation</h3>
+		<form name="form1" method="post" action="hw2.php">
+			<div>
+			<select name="page">
+				<option value="order">New Order Form</option>
+				<option value="edit">Edit Order Form</option>
+				<option value="customer">New Customer Form/Edit Customer</option>
+				<option value="manage">Management Access Only</option>
+				<option value="report">Itemized Sales Report</option>
+				<option value="performance">Performance Report</option>
+				<option value="business">Business Report</option>
+			</select>
+			<input type="hidden" name="acct" value="manager"/>
+			</div>
+			<p><input type="submit" value="Submit" /></p>
+		</form>
+		8:00 a.m. BIT 4444<br/>
+		Group 6 Team Members:<br/>
+			Minahm Kim<br/>
+			Andrew Knittle<br/>
+			Nathan Egbert<br/>
+			Marcella Krzywicki<br/>
+	</div>';}
+	else{print '
+	<h2>User Homepage</h2>
+	<h4>Welcome '.$_SESSION['name'].'</h4>
+	<div id="nav">
+		<h3>Navigation</h3>
+		<form name="form1" method="post" action="hw2.php">
+			<div>
+			<select name="page">
+				<option value="order">New Order Form/Edit Order Form</option>
+				<option value="customer">New Customer Form/Edit Customer</option>
+				<option value="item">Itemize sales report</option>
+			</select>
+			<input type="hidden" name="acct" value="user"/>
+			</div>
+			<p><input type="submit" value="Submit" /></p>
+		</form>
+		8:00 a.m. BIT 4444:<br/>
+		Group 6 Team Members:<br/>
+			Minahm Kim<br/>
+			Andrew Knittle<br/>
+			Nathan Egbert<br/>
+			Marcella Krzywicki
+	</div>';}?>
+	<div id="section">
+		<center>
+			This project has been designed and developed by Minahm Kim, Nathan Egbert, Andrew Knittle, and Marcella Krzywicki.
+		</center>
+		Building a web based decision support system (DSS) to assist managers and sales staff at Piedmont Furnishings with sale orders, customer forms, 
+		itemized sales report, and performance/business analysis reports. This web based DSS is designed to help the day-to-day activities at Piedmont 
+		Furnishings be more organized and streamline sales for the managers and sales staff to prevent errors and increase customer’s satisfaction. 
+	</div>
+	<h5>Last Modified: 9/26/2014</h5>
+</body>
+</html>
--- hw2.php
+++ hw2.php
@@ -12,34 +12,34 @@
 	$account = $_POST['acct'];
 	switch($page){
 	case 'user':
-		header("Location: userpage.html");
+		header("Location: home.php");
 		break;
 	case 'manager':
-		header("Location: managerpage.html");
+		header("Location: home.php");
 		break;
 	case 'order':
-		header("Location: new_order_form.php?acct=".$account);
+		header("Location: new_order_form.php");
 		break;
 	case 'edit':
-		header("Location: edit_order_form.php?acct=".$account);
+		header("Location: edit_order_form.php");
 		break;
 	case 'item':
-		header("Location: item.php?acct=".$account);
+		header("Location: item.php?acct=");
 		break;
 	case 'customer':
-		header("Location: new_customer_form.php?acct=".$account);
+		header("Location: new_customer_form.php");
 		break;
 	case 'manage':
-		header("Location: manage.php?acct=".$account);
+		header("Location: manage.php");
 		break;
 	case 'report':
-		header("Location: report.php?acct=".$account);
+		header("Location: report.php");
 		break;
 	case 'performance':
-		header("Location: performanceData.php?acct=".$account);
+		header("Location: performanceData.php");
 		break;
 	case 'business':
-		header("Location: businessData.php?acct=".$account);
+		header("Location: businessData.php");
 		break;
 	default:
 	}
