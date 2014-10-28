fba11bd9eed6fcee15ffe73f4bb629e34325ea6e
minahm <mdkim105@gmail.com>
2014-10-27 20:35:30 -0800

    ANdREW STOP DELETING MY SESSIONS

--- customer_result.php
+++ customer_result.php
@@ -9,6 +9,63 @@
 <body>
 	<h1><a href="http://tinyurl.com/mstgdqk"><img src="http://tinyurl.com/on58dwh" alt=" photo Untitled_zps8bfcff57.jpg"/></a></h1>
 	<hr/>
+	<h2> Customer Submitted:</h2>
+		<?if($_SESSION['account'] == "manager"){print '
+	<h4>Welcome '.$_SESSION['name'].'</h4>
+	<div id="nav">
+		<h3>Navigation - Manager Portal</h3>
+		<form name="form1" method="post" action="hw2.php">
+			<div>
+			<select name="page">
+				<option value="home">Home</option>
+				<option value="order">New Order Form</option>
+				<option value="edit">Edit Order Form</option>
+				<option value="customer">New Customer Form</option>
+				<option value="edit_customer">Edit Customer</option>
+				<option value="manage">Management Access Only</option>
+				<option value="report">Itemized Sales Report</option>
+				<option value="performance">Performance Report</option>
+				<option value="business">Business Report</option>
+			</select>
+			<input type="hidden" name="acct" value="manager"/>
+			</div>
+			<p><input type="submit" value="Submit" /></p>
+		</form>
+		8:00 a.m. BIT 4444<br/>
+		Group 6 Team Members:<br/>
+			Minahm Kim<br/>
+			Andrew Knittle<br/>
+			Nathan Egbert<br/>
+			Last Modified: 10/27/2014
+	
+	</div>';}
+	else{print '
+
+	<h4>Welcome '.$_SESSION['name'].' <form method="POST" action="signout.php"><input type="submit" value="Logout" /></form><br /></h4>
+	<hr/>
+	<div id="nav">
+		<h3>Navigation - User Portal</h3>
+		<form name="form1" method="post" action="hw2.php">
+			<div>
+			<select name="page">
+				<option value="home">Home</option>
+				<option value="order">New Order Form</option>
+				<option value="edit">Edit Order</option>
+				<option value="customer">New Customer Form</option>
+				<option value="edit_customer">Edit Customer</option>
+				<option value="item">Itemize sales report</option>
+			</select>
+			<input type="hidden" name="acct" value="user"/>
+			</div>
+			<p><input type="submit" value="Submit" /></p>
+		</form>
+		8:00 a.m. BIT 4444:<br/>
+		Group 6 Team Members:<br/>
+			Minahm Kim<br/>
+			Andrew Knittle<br/>
+			Nathan Egbert<br/>
+			Last Modified: 10/27/2014
+	</div>';}?>
 	<?
 		$customer = $_POST['custID'];
 		$region = $_POST['region'];
@@ -71,6 +128,11 @@
 			<td><?print $email?></td>
 		</tr>
     </table>
+	<form method="post" action="hw2.php">
+		<input type="hidden" name="page" value="home"/>
+		</br>
+		<input type="submit" value="Return"/>
+	</form>
 	<h5>Last Modified: 9/26/2014</h5>
 </body>
 </html>
