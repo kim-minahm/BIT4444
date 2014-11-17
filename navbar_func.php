fb9c5fd7eac7efe80bd2e9dbef032d2d56f479dc
whoisjamosses <egbertnd@gmail.com>
2014-11-16 22:15:59 -0500

    wabaki

--- navbar_func.php
+++ navbar_func.php
@@ -1,11 +1,11 @@
 <?
 	function navbar()
 	{
-	if($_SESSION['account'] == "manager"){print '
-	
-	<h4>Welcome '.$_SESSION['name'].' <form method="POST" action="signout.php"><input type="submit" value="Logout" /></form><br /></h4>
-	<hr/>
-	<h2>Manager Portal</h2>
+	<?if($_SESSION['account'] == "manager"){print '
+
+	<h4>Welcome '.$_SESSION['name'].' <form method="POST" action="signout.php"><input type="submit" value="Logout" /></form></h4>
+<hr/>
+<h2>New Customer Form</h2>
 	<div id="nav">
 		<h3>Navigation - Manager Portal</h3>
 		<form name="form1" method="post" action="hw2.php">
@@ -31,7 +31,34 @@
 			Andrew Knittle<br/>
 			Nathan Egbert<br/>
 			Last Modified: 10/27/2014
-	
+			
 	</div>';}
-	}
+	else{print '
+
+	<h4>Welcome '.$_SESSION['name'].' <form method="POST" action="signout.php"><input type="submit" value="Logout" /></form><br /></h4>
+<hr/>
+<h2>New Customer Form</h2>
+	<div id="nav">
+		<h3>Navigation - Employee Portal</h3>
+		<form name="form1" method="post" action="hw2.php">
+			<div>
+			<select name="page">
+				<option value="home">Home</option>
+				<option value="order">New Order Form</option>
+				<option value="edit">Edit Order</option>
+				<option value="customer">New Customer Form</option>
+				<option value="edit_customer">Edit Customer</option>
+				<option value="item">Itemize sales report</option>
+			</select>
+			<input type="hidden" name="acct" value="user"/>
+			</div>
+			<p><input type="submit" value="Submit" /></p>
+		</form>
+		8:00 a.m. BIT 4444:<br/>
+		Group 6 Team Members:<br/>
+			Minahm Kim<br/>
+			Andrew Knittle<br/>
+			Nathan Egbert<br/>
+			Last Modified: 10/27/2014
+	</div>';}
 ?>
